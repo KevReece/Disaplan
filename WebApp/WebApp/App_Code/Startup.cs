a1936a1e59ee4861386d93961d0250aef186891c
KevReece <kevinjamesreece@gmail.com>
2018-08-31 15:56:53 +0200

    initial WebApp

---
+++ WebApp/WebApp/App_Code/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+public class Startup
+{
+    public void Configuration(IAppBuilder app)
+    {
+        app.UseCookieAuthentication(new CookieAuthenticationOptions
+        {
+            AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+            LoginPath = new PathString("/Login")
+        });
+    }
+}
